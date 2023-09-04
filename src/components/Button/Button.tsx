7c9bb163585e9065532063fe801fb971a8dff62b
Snezh88k <snezh88k@gmail.com>
2023-09-04 19:02:06 +0300

    change button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,5 +3,5 @@ import React from "react";
 export interface ButtonProps {}
 
 export default function Button(props: ButtonProps) {
-  return <div>Button</div>;
+  return <div>Кнопка из библиотеки</div>;
 }
